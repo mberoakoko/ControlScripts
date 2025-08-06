2fe29bcad55b2f72b92483f751614e46ab5814f9
mbero_akoko <mberoakoko@gmail.com>
2025-08-06 12:51:05 +0200

    Initial Draft for the noisy plant

--- src/steering_control/models/closed_loop_plants.py
+++ src/steering_control/models/closed_loop_plants.py
@@ -28,6 +28,32 @@ class VehiclePlant:
         )
 
 
+    def __post_init__(self):
+        self.controller = LQRController(
+            linearized_plant=self.plant.as_linear_stata_space(
+                x_equilibrium=np.array([10, 0, 0]),
+                u_equilibrium=np.array([10, 0])
+            )
+        ).as_non_linear_io_system_noisy_block()
+
+    def create_closed_loop_system(self) -> control.NonlinearIOSystem:
+        nominal_plant = self.plant.as_non_linear_system()
+        noise_block = NoiseBlock().as_non_linear_io_system()
+        noise_plant = control.interconnect(
+            [nominal_plant, noise_block],
+            states=nominal_plant.state_labels,
+            inputs=nominal_plant.input_labels,
+            outputs=noise_block.output_labels
+        )
+        print(noise_plant)
+        print("---"*20)
+        print(self.controller)
+        return control.interconnect(
+            [noise_plant, self.controller],
+            states=["x", "y", "theta"],
+            inputs=["x_d","y_d", "theta_d", "v_d", "delta_d",],
+            outputs=['x_n', 'y_n', 'theta_n', "delta"]
+        )
 
 
 if __name__ == "__main__":
