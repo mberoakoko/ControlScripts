dd6a0c3f98ab85fe355bc1cdb5b5e0f8c6f9bd07
mbero_akoko <mberoakoko@gmail.com>
2025-08-09 23:54:05 +0200

    Implemented FilterBlock

--- src/inverted_pendulum_control/observers/linear_time_invariant_filtering.py
+++ src/inverted_pendulum_control/observers/linear_time_invariant_filtering.py
@@ -23,6 +23,39 @@ class FilterChannel:
         out, new_z_i = signal.lfilter(self.b, self.a, x, zi=self.z_i)
         return new_z_i.squeeze(), out.squeeze()
 
+@dataclasses.dataclass
+class FilterBlock:
+    x_channel_filter: FilterChannel = FilterChannel(*butter_filter_factory(taps=BLOCK_TAPS))
+    v_channel_filter: FilterChannel = FilterChannel(*butter_filter_factory(taps=BLOCK_TAPS))
+    theta_channel_filter: FilterChannel =  FilterChannel(*butter_filter_factory(taps=BLOCK_TAPS))
+    theta_dot_channel_filter: FilterChannel = FilterChannel(*butter_filter_factory(taps=BLOCK_TAPS))
+
+
+    def __update_block_state(self, t, x: np.ndarray, u: np.ndarray, params) -> np.ndarray:
+        x_val, v_val, theta_val, theta_dot_val = u
+        x_filtered = self.x_channel_filter(t, np.array([x_val]), None, None),
+        v_Filtered = self.v_channel_filter(t, np.array([v_val]), None, None),
+        theta_filtered = self.theta_channel_filter(t, np.array([theta_val]), None, None)
+        theta_dot_filtered = self.theta_dot_channel_filter(t, np.array([theta_dot_val]), None, None)
+        print(f"{theta_dot_filtered=}")
+        return np.array([
+            x_filtered[0],
+            v_Filtered[0],
+            theta_filtered,
+            theta_dot_filtered
+        ])
+
+    def __output_block_state(self, t, x: np.ndarray, u: np.ndarray, params) -> np.ndarray:
+        return x
+
+    def as_non_linear_io_system(self) -> control.NonlinearIOSystem:
+        return control.NonlinearIOSystem(
+            self.__update_block_state, self.__output_block_state,
+            name="FilterBlock",
+            states=["x_hat", "v_hat", "theta_hat", "theta_dot_hat"],
+            inputs=["x", "v", "theta", "theta_dot"],
+            outputs=["x_hat", "v_hat", "theta_hat", "theta_dot_hat"],
+        )
 
 if __name__ == "__main__":
     b, a = butter_filter_factory(taps=10)
