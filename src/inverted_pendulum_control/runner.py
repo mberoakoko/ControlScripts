fb587f058230d3b881062028338f452ca29e03bb
mbero_akoko <mberoakoko@gmail.com>
2025-08-09 13:35:08 +0200

    Initial test simulation

--- src/inverted_pendulum_control/runner.py
+++ src/inverted_pendulum_control/runner.py
@@ -13,3 +13,33 @@ if __name__ == "__main__":
     simple_stabilizing_plot(
         response_data=response
     )
+    response.to_pandas().to_csv(save_location)
+
+def test_least_squares_filter() -> None:
+
+    def _load_data() -> pd.DataFrame:
+        save_location = pathlib.Path(__file__).parent / "data/stabilization_simulation.csv"
+        return pd.read_csv(save_location)
+
+    test_inverted_pendulum = InvertedPendulum()
+    pend_linarized = linearize_plant(test_inverted_pendulum)
+    filter_obj = LeastSquaresFilter(
+        plant_discr=pend_linarized
+    )
+    filter = filter_obj.as_non_linear_io_system()
+    print(filter)
+    print("\n")
+
+    test_x = np.ones(4)
+    filter.dynamics(0, test_x, test_x, None)
+    data_raw = _load_data()
+    columns = ["x", "v", "theta", "theta_dot"]
+    t = data_raw["time"].to_numpy()
+    data: np.ndarray = data_raw.loc[:, columns].to_numpy().T
+    filter_result = control.input_output_response(filter, t,  data, initial_state=data[:, 0])
+    print(filter_result.to_pandas().head())
+    filter_result.to_pandas().to_csv(pathlib.Path(__file__).parent / "data/test_least_squares_filter.csv")
+
+
+if __name__ == "__main__":
+    test_least_squares_filter()
