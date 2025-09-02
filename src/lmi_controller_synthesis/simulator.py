5f0b94c02e0fda99af02cd08c3b58170daeffa94
mbero-akoko <mberoakoko@gmail.com>
2025-09-03 00:19:01 +0200

    closed loop system works

--- src/lmi_controller_synthesis/simulator.py
+++ src/lmi_controller_synthesis/simulator.py
@@ -1,3 +1,29 @@
 import control
 
 _SIMULATION_DT: float = 0.001
+
+def create_lower_star_mass_spring_damper() -> control.NonlinearIOSystem:
+    raw_plant = MassSpringDamperExogenous(
+        m=1,
+        c=1,
+        k=1,
+        alpha_1=1,
+        alpha_2=1,
+    )
+    print(raw_plant.A.shape)
+    raw_controller_full_state = LowerStarController(
+        params=control.ss(
+            np.zeros_like(raw_plant.A),
+            np.zeros((2, 2)),
+            np.zeros((2, 2)),
+            np.ones((2, 2))
+        )
+    )
+    plant = raw_plant.as_non_linear_io_system()
+    ctrl_plant = raw_controller_full_state.as_non_linear_io_system()
+    closed_Loop_system = control.interconnect([plant, ctrl_plant], inputs=["w",], outputs=["z", ])
+    return closed_Loop_system
+
+
+if __name__ == "__main__":
+    print(create_lower_star_mass_spring_damper())
