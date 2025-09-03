616e59469410d15b56c45005abc85967387a22dc
mbero-akoko <mberoakoko@gmail.com>
2025-09-03 09:42:54 +0200

    controller set up

--- src/lmi_controller_synthesis/controllers_and_observers/robust_controllers.py
+++ src/lmi_controller_synthesis/controllers_and_observers/robust_controllers.py
@@ -3,7 +3,9 @@ import dataclasses
 import enum
 import numpy as np
 
-
+class ControllerType(enum.IntEnum):
+    FULL_STATE_CONTROLLER = 0
+    DYNAMIC_UPDATE_CONTROLLER = 1
 
 @dataclasses.dataclass
 class LowerStarController:
@@ -12,17 +14,40 @@ class LowerStarController:
     def __controller_update(self, t: float, x: np.ndarray, u: np.ndarray, params: dict) -> np.ndarray:
         return self.params.A @ x + self.params.B @ u
 
-    def __controller_output(self, t: float, x: np.ndarray, u: np.ndarray, params: dict) -> np.ndarray:
-        print(f"{x=}")
-        print(f"{x.shape=}")
+    def __controller_full_state_output(self, t: float, x: np.ndarray, u: np.ndarray, params: dict) -> np.ndarray:
+        print(f"{self.params.D=}")
+        print(f"{u=}")
+        return self.params.D @ u
+
+    def __controller_dynamic_output(self, t: float, x: np.ndarray, u: np.ndarray, params: dict) -> np.ndarray:
         return self.params.C @ x + self.params.D @ u
 
-    def as_non_linear_io_system(self, contoller_inputs: list[str] = ("y_1", "y_2" ), controller_outputs: list[str] = ("u", )) -> control.NonlinearIOSystem:
-        return control.NonlinearIOSystem(
-            self.__controller_update, self.__controller_output,
-            name="LowerStarController",
-            states=["x_ctrl"],
-            inputs=contoller_inputs,
-            outputs=controller_outputs
-        )
+    def __controller_factory(self,
+                             controller_type: ControllerType,
+                             controller_inputs: list[str],
+                             controller_outputs: list[str],
+                             ) -> control.NonlinearIOSystem:
+        match controller_type:
+            case ControllerType.FULL_STATE_CONTROLLER:
+                return control.NonlinearIOSystem(
+                    None, self.__controller_full_state_output,
+                    name="LowerStarControllerFullState",
+                    inputs=controller_inputs,
+                    outputs=controller_outputs
+                )
+
+            case ControllerType.DYNAMIC_UPDATE_CONTROLLER:
+                return control.NonlinearIOSystem(
+                    self.__controller_update, self.__controller_full_state_output,
+                    name="LowerStarControllerDynamicUpdate",
+                    states=["x_ctrl"],
+                    inputs=controller_inputs,
+                    outputs=controller_outputs
+                )
+
+    def as_non_linear_io_system(self,
+                                contoller_inputs: list[str] = ("y_1", "y_2" ),
+                                controller_outputs: list[str] = ("u", ),
+                                controller_type: ControllerType = ControllerType.FULL_STATE_CONTROLLER) -> control.NonlinearIOSystem:
+        return self.__controller_factory(controller_type, contoller_inputs, controller_outputs, )
 
